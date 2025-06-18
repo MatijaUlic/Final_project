ecade6f39b625c9d2ba4dd83de9d98d9a65861f8
SooUnlucky <timon.reding@bluewin.ch>
2025-06-18 19:02:45 +0200

    Added full  CI/CD pipeline for actions test 4

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,14 +2,12 @@
 
 import os
 import sys
-
-# Make sure tests can find src directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-
 import pandas as pd
 import joblib
 from sklearn.dummy import DummyClassifier
 from src.predict import main as predict_main
+# Make sure tests can find src directory
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
 
 def test_predict_creates_outputs(tmp_path, monkeypatch, capsys):
